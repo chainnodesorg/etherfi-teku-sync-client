495187df715bdcf5ef51027e3d0b015afb39663d
Koray Koska <11356621+koraykoska@users.noreply.github.com>
2023-11-24 05:36:46 +0100

    feat: 1000 limit

--- src/subgraph.js
+++ src/subgraph.js
@@ -3,7 +3,7 @@ import { request, gql } from 'graphql-request';
 export const retrieveBidsFromSubgraph = async (GRAPH_URL, BIDDER) => {
   const bidsQuery = gql`
     {
-      bids(where: { bidderAddress: "${BIDDER}", status: "WON", validator_not: null, validator_: { phase: LIVE} }) {
+      bids(first: 1000, where: { bidderAddress: "${BIDDER}", status: "WON", validator_not: null, validator_: { phase: LIVE} }) {
         id
         bidderAddress
         pubKeyIndex
@@ -24,7 +24,7 @@ export const retrieveBidsFromSubgraph = async (GRAPH_URL, BIDDER) => {
     const { bids: result } = await request(GRAPH_URL, bidsQuery);
     bids = result;
   } catch (error) {
-    console.log(error)
+    console.log(error);
     console.error('an error occurred querying bids');
   }
   return bids;
